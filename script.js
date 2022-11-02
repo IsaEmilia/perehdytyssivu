31294c6bc438d51ea6a0bd75e1ca905b13b80644
Isa Hänninen <55230101+IsaEmilia@users.noreply.github.com>
2022-11-02 12:51:06 +0200

    Create styles and fight with js.

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-const button = document.getElementsByClassName("infobutton")
-const content = document.getElementsByClassName("info-osio")
-button.addEvenListener('click', openSesame())
 
-function openSesame() {
-    content.classList.toggle('show')
-    console.log("Teen asioita")
-}
+const title = document.getElementsByClassName('info-header')
+const content = document.getElementsByClassName('info-content')
 
-openSesame()
-
+    for(let i=0; i < title.length; i++){
+        title[i].addEventListener("click", function() {
+            console.log("click")
+            
+                for(let i=0; i < content.length; i++){
+                    content[i].classList.toggle('active')
+                    console.log("activated")
+                }
+        })
+    }
 console.log("toiminnassa")
+
+
+
+/*title.addEventListener('click', () => {
+        const content = document.getElementById("osio1")
+        const arrow = document.getElementById("info-arrow")
+        content.classList.toggle("active-info")
+        title.classList.toggle("active-color")
+        arrow.classList.toggle("info-arrow-active")
+        console.log("click")
+    } )
+
+console.log("toiminnassa")*/
+
+
+/*document.querySelector('.info-header').addEventListener('click', function() {
+    this.classList.toggle('active')
+    this.classList.toggle('active-color')
+    document.querySelector('.info-osio').classList.toggle('active-info-osio')
+})*/
