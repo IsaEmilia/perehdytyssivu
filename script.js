fb22224e4d0821d30a32358f7b21041520df379a
Isa Hänninen <55230101+IsaEmilia@users.noreply.github.com>
2022-11-02 15:15:39 +0200

    Remove useless files.

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ const content = document.getElementsByClassName('info-osio')
             
         })
     }
+
+function onKonamiCode(cb) {
+    var input = '';
+    var key = '38384040373937396665';
+    document.addEventListener('keydown', function (e) {
+        input += ("" + e.keyCode);
+        if (input === key) {
+        return cb();
+        }
+        if (!key.indexOf(input)) return;
+        input = ("" + e.keyCode);
+    });
+    }
+    
+    onKonamiCode(function () {alert('\o/')})
